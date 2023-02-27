3388e0a9d519003aed300fc80fec2b324581c8b1
Gabriele <g.nocchi@hotmail.com>
2023-02-27 10:59:14 -0700

    Add files via upload

--- 4_remove_dup.sh
+++ 4_remove_dup.sh
@@ -1,10 +1,9 @@
-
 #!/bin/bash
 #SBATCH --time=0-6:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=1G
 #SBATCH --account=def-yeaman
-#SBATCH --array=1-83
+#SBATCH --array=1-2
 
 INPUT=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list1.txt)
 OUTPUT=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list2.txt)
