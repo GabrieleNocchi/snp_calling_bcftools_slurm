994ec86063cdff29f18f5c9b25ca7d5003cc1837
Gabriele <g.nocchi@hotmail.com>
2023-02-28 11:23:32 -0700

    Add files via upload

--- 8_mpileup.sh
+++ 8_mpileup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=20G
-#SBATCH --time=00-12:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
+#SBATCH --time=00-23:00:00
 #SBATCH --account=def-yeaman
 #SBATCH --array=1-14
 
@@ -14,4 +14,4 @@ CHROM=$(sed -n "${SLURM_ARRAY_TASK_ID}p" chromosomes.txt)
 
 
 
-parallel -j8 "bcftools mpileup -Ou -f Betula_pendula_subsp._pendula.fa --bam-list list.txt -q 5 -r $CHROM -I -a FMT/AD | bcftools call -S ploidymap.txt -G - -f GQ -mv -Ov > $CHROM\.vcf"
+bcftools mpileup -Ou -f Betula_pendula_subsp._pendula.fa --bam-list list.txt -q 5 -r $CHROM -I -a FMT/AD | bcftools call -S ploidymap.txt -G - -f GQ -mv -Ov > $CHROM\.vcf
