d7eca99876e880a7f255112279bd576665143c51
Gabriele <g.nocchi@hotmail.com>
2023-07-14 11:30:57 -0600

    Update 9_concat.sh

--- 9_concat.sh
+++ 9_concat.sh
@@ -2,15 +2,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=16G
-#SBATCH --time=00-23:00:00
+#SBATCH --mem=32G
+#SBATCH --time=06-00:00:00
 #SBATCH --account=def-yeaman
 
-module load vcftools bcftools
-module load StdEnv/2020 intel/2020.1.217 tabix/0.2.6
-
-vcf-concat Bpe_Chr1.vcf Bpe_Chr2.vcf Bpe_Chr3.vcf Bpe_Chr4.vcf Bpe_Chr5.vcf Bpe_Chr6.vcf Bpe_Chr7.vcf Bpe_Chr8.vcf Bpe_Chr9.vcf Bpe_Chr10.vcf Bpe_Chr11.vcf Bpe_Chr12.vcf Bpe_Chr13.vcf Bpe_Chr14.vcf > nocchi_bplaty.vcf
+module load bcftools
 
 
-bgzip nocchi_bplaty.vcf
-tabix -p vcf nocchi_bplaty.vcf.gz
+bcftools concat -f list.txt -Oz > spruce_banff_2.vcf.gz
+tabix -p vcf spruce_banff_2.vcf.gz
